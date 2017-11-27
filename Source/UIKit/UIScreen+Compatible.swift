9853dacab4c10d541280dc0a41fd247fdbef6374
PangHu <djangolee@sina.com>
2017-11-27 23:38:18 +0800

    add UIScreen+Compatible

---
+++ Source/UIKit/UIScreen+Compatible.swift
@@ -0,0 +1,14 @@
+//
+//  UIScreen+Compatible.swift
+//  JoUIKit
+//
+//  Created by Panghu Lee on 27/11/2017.
+//  Copyright © 2017 Django Lee. All rights reserved.
+//
+
+extension JoUIKit where Base: UIScreen {
+    
+    public var apixel: CGFloat {
+        return 1 / base.scale
+    }
+}
